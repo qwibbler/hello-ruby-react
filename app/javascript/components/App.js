7b9eadcc41464956125c428072249df270002a2b
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-08-08 18:47:02 -0600

    Start Redux

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,24 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from '../configureStore';
 
-import HelloWorld from "./HelloWorld"
+import HelloWorld from './HelloWorld';
 
+const store = configureStore();
 class App extends React.Component {
-  render () {
+  render() {
     return (
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<h1>Hello World</h1>} />
-          <Route path="/hello" element={<HelloWorld greeting="Friend" />} />
-        </Routes>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<h1>Hello World</h1>} />
+            <Route path="/hello" element={<HelloWorld greeting="Friend" />} />
+          </Routes>
+        </Router>
+      </Provider>
     );
   }
 }
 
-export default App
+export default App;
--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,10 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect"
+
+const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST'
+
+function getThings() {
+  console.log('ACTION: getThings')
+  return {
+    type: GET_THINGS_REQUEST,
+  }
+}
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
+        <button className="getThings" onClick={this.props.getThings}>Get Things</button>
       </React.Fragment>
     );
   }
@@ -13,4 +25,11 @@ class HelloWorld extends React.Component {
 HelloWorld.propTypes = {
   greeting: PropTypes.string
 };
-export default HelloWorld
+
+const structuredSelector = createStructuredSelector({
+  things: state => state.things,
+})
+
+const mapDispatchToProps = { getThings };
+
+export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
